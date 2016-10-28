c791ae23080f1eb5d7df2000340499093b1eecc8
Kuntau <kuntau17@gmail.com>
2016-10-29 05:50:08 +0800

    update usercard

--- src/components/Usercard.vue
+++ src/components/Usercard.vue
@@ -2,8 +2,22 @@
   <div class="usercard">
     <img :src="user.avatar_url" :alt="user.name" />
     <div class="top">
-      <p>{{ user.name }}</p>
-      <p>{{ user.name }}</p>
+      <div class="info">
+        <h2>{{ user.name }}</h2>
+        <p>@{{ user.login }}</p>
+      </div>
+      <div class="widget">
+        <p class="title">Repositories</p>
+        <span class="count">{{ repos }}</span>
+      </div>
+      <div class="widget">
+        <p class="title">Follower</p>
+        <span class="count">{{ followers }}</span>
+      </div>
+      <div class="widget">
+        <p class="title">Following</p>
+        <span class="count">{{ following }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -12,10 +26,46 @@
 export default {
   name: 'usercard',
   props: [ 'user' ],
-  data () {
-    return {
-      username: 'Kuntau',
-      avatar: 'https://avatars.githubusercontent.com/u/327489?v=3',
+  data: () => ({
+    followers: '0',
+    following: '0',
+    repos: '0',
+  }),
+  beforeUpdate() {
+    this.getCount("followers", this.user.followers_url)
+    this.getCount("following", this.user.following_url)
+    this.getCount("repos", this.user.repos_url)
+  },
+  beforeMount() {
+    this.getCount("followers", this.user.followers_url)
+    this.getCount("following", this.user.following_url)
+    this.getCount("repos", this.user.repos_url)
+  },
+  methods: {
+    getCount(type, url) {
+      this.$http.get(url)
+      .then((res) => {
+        const result = res.body
+        // console.log("HTTP CALL ", result)
+        switch (type) {
+          case "followers":
+            this.followers = result.length
+            break
+          case "following":
+            this.following = result.length
+            break
+          case "repos":
+            this.repos = result.length
+            break
+          default:
+            console.log("SWITCH ERROR" , type)
+        }
+        return result
+      })
+      .catch((error) => {
+        console.log("Catch", error)
+        // this.text = error.status
+      })
     }
   }
 }
@@ -31,14 +81,34 @@ export default {
   background-color white
   border-radius 2px
   box-shadow 1px 1px 10px rgba(0,0,0,.1), 10px 10px 50px rgba(0,0,0,.2), 20px 20px 100px rgba(0,0,0,.3)
+  color #333
 img
-  height 50px
-  width  50px
+  height 70px
+  width  70px
   border-radius 50%
 .top
   display flex
-  height 50px
-  margin-left 20px
-  h2
-    margin auto
+  height 70px
+  width 530px
+  margin 0 0 20px 20px
+  border-bottom 1px solid #999
+  .info
+    margin auto 0
+    padding 0
+    margin-right auto
+    h2,p
+      display block
+      margin 0
+      text-align left
+  .widget
+    margin auto 10px
+    padding 0
+    align right
+    p
+      text-transform uppercase
+      font-size 8px
+      margin 0
+    span
+      font-size 22px
+      font-weight bold
 </style>
