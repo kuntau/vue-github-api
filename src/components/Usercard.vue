b66684f57d4249a029abf9c93ffad5dbef3c900f
Kuntau <kuntau17@gmail.com>
2016-10-31 03:28:18 +0800

    add repo listing and repo details functionality

--- src/components/Usercard.vue
+++ src/components/Usercard.vue
@@ -1,11 +1,5 @@
-<template lang="html">
 <template>
   <div class="usercard">
-    <img :src="user.avatar_url" :alt="user.name" />
-    <div class="top">
-      <div class="info">
-        <h2>{{ user.name }}</h2>
-        <p>@{{ user.login }}{{ user.company ? " / " + user.company : "" }}</p>
     <div class="row_first">
       <a :href="user.html_url" target="_blank">
         <img :src="user.avatar_url" :alt="user.name" />
@@ -41,9 +35,24 @@
         <p v-if="user.location"><strong>Location</strong> {{ user.location }}</p>
         <p v-if="user.bio"><strong>Biodata</strong> {{ user.bio }}</p>
       </div>
-      <div class="widget">
-        <p class="title">Total Following</p>
-        <span class="count">{{ user.following }}</span>
+      <div class="repos" v-if="current_view === 'repos_view'">
+        <div class="repos_list">
+          <ul v-if="cache.repos.length > 0">
+            <li v-for="(repo, index) in cache.repos">
+              <a @click="reposMore(index)" class="cursor">
+                {{ repo.name }}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div v-if="cache.repos_more_view" class="repos_more">
+          <ul>
+            <li><strong>Repositories Details :</strong> {{ cache.repos_index }}</li>
+            <li><b>Star</b> {{ cache.repos[cache.repos_index].stargazers_count }}</li>
+            <li><b>Watcher</b> {{ cache.repos[cache.repos_index].watchers_count }}</li>
+            <li><b>Fork</b> {{ cache.repos[cache.repos_index].forks_count }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -58,23 +67,38 @@ export default {
     clientSecret: String
   },
   data: () => ({
+    current_view: "user_view",
+    last_call: {
+      repos: {},
+      followers: {},
+      following: {},
+    },
+    cache: {
+      repos: {},
+      repos_more: {},
+      repos_index: 0,
+      repos_more_view: false,
+      followers: {},
+      following: {},
+    },
   }),
-  beforeUpdate() {
-    // this.getCount("followers", this.user.followers_url)
-    // this.getCount("following", this.user.following_url)
-    // this.getCount("repos", this.user.repos_url)
-  },
-  beforeMount() {
-    // this.getCount("followers", this.user.followers_url)
-    // this.getCount("following", this.user.following_url)
-    // this.getCount("repos", this.user.repos_url)
-  },
   methods: {
     getCount(type, url) {
-      this.$http.get(url + "?client_id=" + clientID + "&client_secret="  + clientSecret)
+      this.current_view = type
+      if ((!url) || // check if url is null/empty
+        (url === this.last_call.repos) || // prev url call check, thus limit the api call
+        (url === this.last_call.followers) ||
+        (url === this.last_call.following))
+        return
+
+      // if the url was never called, clear previous obejct
+      this.cache.repos = {}
+
+      // xhr http request using promise
+      this.$http.get(url + "?client_id=" + this.clientID + "&client_secret="  + this.clientSecret)
+      // this.$http.get(url)
       .then((res) => {
         const result = res.body
-        // console.log("HTTP CALL ", result)
         switch (type) {
           case "followers":
             this.followers = result.length
@@ -82,18 +106,36 @@ export default {
           case "following":
             this.following = result.length
             break
-          case "repos":
-            this.repos = result.length
+          case "repos_view":
+            this.cache.repos = result
+            this.last_call.repos = url
             break
           default:
             console.log("SWITCH ERROR" , type)
         }
+        // console.log("HTTP CALL ", type, url, result)
         return result
       })
       .catch((error) => {
         console.log("Catch", error)
         // this.text = error.status
       })
+    },
+    reposMore(i) {
+      if (this.cache.repos_index !== i) {
+        this.cache.repos_more_view = true
+        this.cache.repos_index = i
+      } else {
+        this.cache.repos_more_view = !this.cache.repos_more_view
+      }
+    },
+    reposMoreOld(repo) {
+      if (this.cache.repos_more !== repo) {
+        this.cache.repos_more_view = true
+        this.cache.repos_more = repo
+      } else {
+        this.cache.repos_more_view = !this.cache.repos_more_view
+      }
     }
   }
 }
@@ -103,46 +145,84 @@ export default {
 .usercard
   margin 50px auto
   padding 20px
-  display flex
+  display inline-flex
+  flex-direction column
   width 600px
-  height 300px
   background-color white
   border-radius 2px
   box-shadow 1px 1px 10px rgba(0,0,0,.1), 10px 10px 50px rgba(0,0,0,.2), 20px 20px 100px rgba(0,0,0,.3)
   color #333
-img
-  height 70px
-  width  70px
-  border-radius 50%
-.top
+.row_first
   display flex
-  height 70px
-  width 530px
-  margin 0 0 20px 20px
+  img
+    height 70px
+    width  70px
+    border-radius 50%
+  .top
+    display flex
+    height 70px
+    width 530px
+    margin 0 0 20px 20px
+    border-bottom 1px solid #999
+    .name
+      margin auto 0
+      padding 0
+      margin-right auto
+      h2,p
+        display block
+        margin 0
+        text-align left
+        max-height 30px
+        overflow hidden
+        text-overflow ellipsis
+      h2
+        font-size 20px
+      small
+        text-transform uppercase
+        font-size 8px
+    .widget
+      margin auto 10px
+      padding 0
+      width 60px
+      align right
+      p
+        text-transform uppercase
+        font-size 8px
+        margin 0
+      span
+        font-size 22px
+        font-weight bold
+.row_third
+  display flex
+  margin-left 90px
+  padding-bottom 20px
   border-bottom 1px solid #999
-  .info
+  .name
     margin auto 0
     padding 0
     margin-right auto
-    h2,p
+    p
       display block
       margin 0
       text-align left
-      max-height 30px
-      overflow hidden
-      text-overflow ellipsis
-    h2
-      font-size 20px
-  .widget
-    margin auto 10px
-    padding 0
-    width 60px
-    align right
-    p
-      text-transform uppercase
-      font-size 8px
-      margin 0
-    span
-      font-size 22px
-      font-weight bold
+  .repos
+    text-align left
+    display flex
+    width 100%
+    ul
+      padding-left 20px
+      li
+        list-style square
+    .repos_more
+      margin-left auto
+      border 1px #333 solid
+      height 33%
+      ul
+        padding 0 20px
+        li
+          list-style none
+.cursor
+  cursor pointer
+  &:hover
+    color #66bb6a
 </style>
