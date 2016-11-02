532991706727013815440538dc9090c6d9eda07f
Kuntau <kuntau17@gmail.com>
2016-11-03 05:07:31 +0800

    serve favicon

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var server = express()
 const PORT = process.env.PORT || 8080
 
 server.get('/', (req, res) => {
-  server.use(express.static('/dist'))
+  // server.use(express.static('/dist'))
   res.sendFile(__dirname + '/index.html')
 })
 
 server.get('/dist/build.js', (req, res) => {
   res.sendFile(__dirname + '/dist/build.js')
+})
+
+server.get('/favicon.ico', (req, res) => {
+  res.sendFile(__dirname + '/favicon.ico')
 })
 
 server.listen(PORT, () => {
