7eb3c722cac0a480c33855bb1a965d753202ab0d
Kuntau <kuntau17@gmail.com>
2016-11-03 04:55:09 +0800

    add express and configure node to serve our index.html

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var server = express()
+const PORT = process.env.PORT || 8080
+
+server.get('/', (req, res) => {
+  server.use(express.static('/dist'))
+  res.sendFile(__dirname + '/index.html')
+})
+
+server.get('/dist/build.js', (req, res) => {
+  res.sendFile(__dirname + '/dist/build.js')
+})
+
+server.listen(PORT, () => {
+  console.log('Vue.js server listening on port ' + PORT + ' !')
+})
